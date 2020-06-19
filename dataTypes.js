4e08150c9388bc64a6dcc1f1206574c6585c545a
Yue Liu <yue.liu@authright.com>
2020-06-19 19:34:17 +0000

    updated: datatypes and functions

---
+++ dataTypes.js
@@ -0,0 +1,30 @@
+/*
+Data Types
+Primitives & Objects
+*/
+
+// 1. boolean
+console.log(true, false);
+// 2. null
+// case-sensitive
+console.log(null);
+// 3. undefined
+console.log(undefined);
+// 4. number
+console.log(42, 3.1415);
+// 5. bitint
+// console.log(9007199254740991n);
+// 6. string
+console.log("Howdy");
+// 7. Symbol
+// immutable
+let sym = Symbol('foo');
+typeof sym;
+console.log(sym);
+// 8. Object
+obj = {
+  html: "html5",
+  css: "css3",
+  javascript: "REACT&REDUX"
+}
+console.log(obj);
---
+++ functions.js
@@ -0,0 +1,65 @@
+/* 
+Functions
+1. building blocks
+2. a JS procedure: a set of statements
+*/
+
+
+// 1. define functions
+// function declarations
+function square(num) { // consisting of 
+  return num * num;    // the function keyword
+}
+// function expressions
+// passing a function as an argument to another function
+const myObj = function myFunc(theObject) { // providing a name
+  theObject.make = 'Toyota';
+}
+// var myFunc; 
+// if (num == 0) {
+//   myFunc = function(theObject) {
+//     theObject.make = 'Toyota';
+//   }
+// }
+
+// 2. calling functions
+// hoisting
+const f = function(x) {
+  return x * x * x;
+}
+let numbers = [0, 1, 2, 5, 10];
+let cube = map(f,numbers); // the function declarations
+console.log(cube); // can appear below the call
+function map(f, a) {
+  let result = [];
+  let i;
+  for (i = 0; i != a.length; i++) {
+    result[i] = f(a[i]);
+  }
+  return result;
+}
+// calling itself
+const fractorial = function frac(n) {
+  return n < 2 ? 1 : n * frac(n-1);
+}
+// calling dynamically
+
+// 3. function scope
+// in a global scope
+var num1 = 20,
+    num2 = 3,
+    name = 'Chamahk';
+function multiply() {
+  return num1 * num2;
+}
+console.log(multiply()); // Returns 60
+// in a function scope
+function getScore() { // A nested function example
+  var num1 = 2,
+      num2 = 3;
+  function add() {
+    return name + ' scored ' + (num1 + num2);
+  }
+  return add();
+}
+console.log(getScore()); // Returns "Chamahk scored 5"
