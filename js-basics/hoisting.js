7e747fd9ac0e4344875414630c7f8db1c7f64176
yue <yue.liu@authright.com>
2020-09-27 18:40:39 -0400

    hoisting

--- js-basics/hoisting.js
+++ js-basics/hoisting.js
@@ -1,9 +1,15 @@
 /*
+Hoisting
+
 Hoisting is a term in ECMA2015.
+
 Hoisting is about how execution contexts work in JavaScript.
 Conceptually, hoisting suggests that variable and function declarations are physically moved to the top of your code.
+
 In fact, the variable and function declarations are put into memory during the compile phase,
 but stay exactly where you typed them in your code.
+
+It means that var, const and let variable declarations are interpreted as if it is at the top of its scope.
 */
 
 // 1. Technical Example
@@ -15,3 +21,15 @@ function catName(name) {
 // 2. Only Declarations Are Hoisted
 var num = 6;
 console.log(num);
+
+// 3.  output?
+// a will be hoisted to the top of its local scope with an initial value of undefined
+var a = 'hi';
+console.log(a); // hi
+function myFunction() {
+  var a;
+  console.log(a); // undefined
+  a = 'hello';
+  console.log(a); // hello
+};
+myFunction();
--- js-core/execution-context.js
+++ js-core/execution-context.js
@@ -1,5 +1,37 @@
 /*
 Exexution Context
 
+Execution context is defined as the environment in which the JavaScript code is executed.
 
+There are three types of Execution Context
+Global Execution Context - Execution Context Stack
+Functional Execution Context
+Eval
+
+Execution Context Stack:
+Order in which functions are called.
+
+JavaScript engine creates the execution context in the following two stages:
+Creation Phase
+Execution Phase
+
+Creation Phase:
+Creates the Activation object or the Variable object
+Creates the scope chain
+Determines the value of this
+
+Scope Chain
+Order in which functions are written lexically.
 */
+
+var a = 10;
+function functionA() {
+  console.log("Start Function A");
+  function functionB() {
+    console.log("In Function B");
+  };
+  functionB();
+};
+
+functionA();
+console.log("Global Context");
