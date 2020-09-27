e4717d6aa998355ccadbde122effdac1050dd96b
yue <yue.liu@authright.com>
2020-09-27 18:45:13 -0400

    execution-context

--- js-core/execution-context.js
+++ js-core/execution-context.js
@@ -35,3 +35,25 @@ function functionA() {
 
 functionA();
 console.log("Global Context");
+
+// output?
+// scope chain
+
+// var a = "Hello!";
+// first();
+
+// function first() {
+//   var b = 'Hi!';
+//   second();
+
+//   function second() {
+//     var c = 'Hey!';
+//     third();
+//   };
+// };
+
+// function third() {
+//   var d = 'John';
+//   // console.log(c);
+//   console.log(a+d);
+// }
