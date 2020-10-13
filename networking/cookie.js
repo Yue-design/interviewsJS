a2914f5f6125f40080ad5e69e2466cd8b019295a
yue <yue.liu@authright.com>
2020-10-13 18:46:48 -0400

    cookie

---
+++ networking/cookie.js
@@ -0,0 +1,13 @@
+/*
+cookie
+
+A cookie is a piece of data that is stored on your computer to be accessed by your browser.
+
+Cookies are saved as key/value pairs.
+
+Cookies are used to remember information about the user profile(such as username).
+
+By default, the cookie is deleted when the browser is closed, and belongs to a current page.
+
+You can delete a cookie by setting the expiry date as a passed date. Also, you should define the cookie path option to ensure that you delete the right cookie.
+*/
