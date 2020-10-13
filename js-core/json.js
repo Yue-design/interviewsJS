e57381d5630d7989628804ff66b4baa8b6dbe4e7
yue <yue.liu@authright.com>
2020-10-13 09:22:45 -0400

    json

---
+++ js-core/json.js
@@ -0,0 +1,12 @@
+/*
+JSON
+
+JSON is a text-based data format following JavaScript object syntax.
+It is useful when transmitting data across a network.
+*/
+
+// 1. parsing: converting a string to a native object
+JSON.parse(text);
+
+// 2. stringification: converting a native object to a string
+JSON.stringify(object);
