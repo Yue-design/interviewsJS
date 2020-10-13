997503e5095f80c47384b1357e67e16f3ff27a43
yue <yue.liu@authright.com>
2020-10-13 15:45:22 -0400

    switch

---
+++ js-basics/switch.js
@@ -0,0 +1,17 @@
+/*
+switch
+
+*/
+
+// redeclare variables in a switch block without an error
+let counter = 1;
+switch(x) {
+  case 0: {
+    let name;
+    break;
+  }
+  case 1: {
+    let name;
+    break;
+  }
+}
