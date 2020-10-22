2b18503166a469debc3abc50b6ad3ca1bcb91b21
yue <yue.liu@authright.com>
2020-10-21 22:12:21 -0400

    delete-typeof

---
+++ js-basics/delete.js
@@ -0,0 +1,8 @@
+/*
+delete property
+*/
+
+var user = { name: "John", age: 20 };
+delete user.age;
+
+console.log(user);
---
+++ js-basics/typeof.js
@@ -0,0 +1,6 @@
+/*
+typeof
+*/
+
+console.log(typeof "John");
+console.log(typeof (1 + 2));
