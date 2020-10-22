38573fb6f9b74caec2898682286124ff574c2e5e
yue <yue.liu@authright.com>
2020-10-21 23:27:47 -0400

    window-history-object

---
+++ networking/history.js
@@ -0,0 +1,13 @@
+/*
+history
+
+window.history is an object contains the browsers history
+*/
+
+function goBack() {
+  window.history.back();
+}
+
+function goForward() {
+  window.history.forward();
+}
