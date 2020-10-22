0aedf35001eee4afa19f64727834f8eef2ad8961
yue <yue.liu@authright.com>
2020-10-21 22:25:18 -0400

    isNaN-eval

--- js-basics/number.js
+++ js-basics/number.js
@@ -1,5 +1,5 @@
 // In JavaScript, numbers are implemented in double-precision 64-bit binary format.
-// IEEE 754: 
+// IEEE 754:
 // +-2**-1022 to +-2**1023
 // +-10**-308 to +-10**308
 // with a numeric precision of 53 bits
@@ -12,15 +12,15 @@
 
 // Four Types of Number Literals
 console.log(42, 123456);
-console.log(0888, 0777); // 888 511 
+console.log(0888, 0777); // 888 511
 // Decimal Numbers
-// Binary Numbers 
+// Binary Numbers
 // Octal Numbers
 // Hexadecimal Numbers
-console.log(0XA); // 10
+console.log(0xa); // 10
 
 // Exponentiation
-console.log(1E3, 0.1e2); // 1000 10
+console.log(1e3, 0.1e2); // 1000 10
 
 // 2. Number Object
 // The built-in Number has properties for numerical constants, and you cannot change the values of these properties.
@@ -41,3 +41,9 @@ console.log(Number.isInteger(3));
 // Methods of Number.Prototype
 let num = 5.123456;
 console.log(num.toPrecision(5), num.toFixed(2)); // 5.1235 5.12
+
+// isNaN()
+console.log(isNaN("hello")); // an illegal number
+
+// eval(): from string to number
+console.log(typeof eval("1+2"));
