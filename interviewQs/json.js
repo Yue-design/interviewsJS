4ac5d0c8ac801a50622d950a541060a846324457
yue <yue.liu@authright.com>
2020-10-30 00:58:33 -0400

    jsonObject-string

--- interviewQs/json.js
+++ interviewQs/json.js
@@ -5,4 +5,12 @@ JSON
 2. what you recive from a web server is always a string
 */
 
-var userJSON;
+// JSON => string
+var userJSON = { name: "John", age: 31 };
+var userString = JSON.stringify(userJSON);
+console.log(userString);
+
+// string => JSON
+var userString = '{ name: "John", age: 31 }';
+var userJSON = JSON.parse(userString);
+console.log(userJSON);
